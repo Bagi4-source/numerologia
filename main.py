a3b14460364e8ef3b6eff4a6aaf2c045a55cff5f
Bagi4 <67656353+Bagi4-source@users.noreply.github.com>
2023-07-15 13:38:49 +0300

    upload project

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ import schemas
 from database import SessionLocal, engine
 from fastapi.security.http import HTTPAuthorizationCredentials, HTTPBearer
 from fastapi.security import OAuth2PasswordRequestForm
+from fastapi.middleware.cors import CORSMiddleware
 from typing import Annotated
 from smtp import send_email
 
@@ -15,6 +16,16 @@ models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Dependency
 def get_db():
